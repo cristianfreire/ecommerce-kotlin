3a2d14e459b1da0f4358958370add7be384074d1
cristianfreire <cristianpassos51@gmail.com>
2020-05-15 11:45:57 -0400

    Login Activity

---
+++ app/src/main/java/com/cristianpassos/salesapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.cristianpassos.salesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        login_signup.setOnClickListener{
+            var i=Intent(this, RegAct::class.java)
+            startActivity(i)
+        }
+    }
+}
